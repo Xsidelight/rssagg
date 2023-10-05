7325ef20c5bec1d04b0eed001098eaf8c94239e5
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2023-10-05 12:14:30 +0600

    aggregation worker

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Xsidelight/rssagg/internal/database"
 	"github.com/go-chi/chi/v5"
@@ -40,6 +41,8 @@ func main() {
 	apiCfg := apiConfig{
 		DB: database.New(conn),
 	}
+
+	go startScraping(apiCfg.DB, 10, time.Minute)
 
 	router := chi.NewRouter()
 
