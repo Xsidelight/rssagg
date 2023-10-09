d1f1b3f2d9f900ec88ccfac6cf32018046105a1a
Tornike Gogberashvili <toko.gogberaa@gmail.com>
2023-10-09 13:12:49 +0600

    implement posts and add them to db

--- scraper.go
+++ scraper.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/Xsidelight/rssagg/internal/database"
+	"github.com/google/uuid"
 )
 
 func startScraping(
@@ -51,7 +54,34 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name)
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123, item.PubDate)
+		if err != nil {
+			log.Println("Error while parsing time", err)
+			continue
+		}
+
+		_, createPostErr := db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if createPostErr != nil {
+			if strings.Contains(createPostErr.Error(), "duplicate key") {
+				continue
+			}
+			log.Println("Error while creating post", createPostErr)
+		}
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 }
